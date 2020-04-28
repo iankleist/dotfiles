218dde9e7f075c41cac9811e8264af516ff769f2
Christian <christian.surala@posteo.net>
2020-04-28 09:00:42 +0200

    New backup 2020-04-28 09:00:42

---
+++ backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# check to see is git command line installed in this machine
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+# copy Vs-Code files
+cp  $HOME/Library/Application\ Support/Code/User/settings.json .
+
+# copy list of extensions that currently installed
+code --list-extensions --show-versions >> ../../vscode/extensions.txt
+
+# copy other dot files
+cp $HOME/.zshrc .
+cp $HOME/.config/nvim/init.vim .
+
+
+# Check git status
+gs="$(git status | grep -i "modified")"
+# echo "${gs}"
+
+# If there is a new change
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+
+# push to Github
+git add -u;
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin master
